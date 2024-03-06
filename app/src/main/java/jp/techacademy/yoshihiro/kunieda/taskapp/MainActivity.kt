847781f9291d4dc194b8e89b03d02cc2f4d85a8f
kunipola <shinshuu.4649@gmail.com>
2024-03-06 11:58:56 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/yoshihiro/kunieda/taskapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package jp.techacademy.yoshihiro.kunieda.taskapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import jp.techacademy.yoshihiro.kunieda.taskapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.fab.setOnClickListener { v ->
+            Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
